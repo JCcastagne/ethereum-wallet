a7b5e8a61e13e84ca501b018ba3ff231a73a168c
JC Castagne <74418183+JCcastagne@users.noreply.github.com>
2022-09-30 11:08:30 -0400

    Remove unused template files, add main html structure.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,104 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <header>Transaction history</header>
+
+      <div className='table'>
+        <div className='row'>
+          <h1 className='date title'>Date</h1>
+          <div className='values'>
+            <p>1000</p>
+            <p>2000</p>
+            <p>3000</p>
+            <p>4000</p>
+            <p>5000</p>
+            <p>6000</p>
+            <p>7000</p>
+            <p>8000</p>
+            <p>9000</p>
+            <p>1000</p>
+          </div>
+        </div>
+        <div className='row'>
+          <h2 className='description title'>Description</h2>
+          <div className='values'>
+            <p>1000</p>
+            <p>2000</p>
+            <p>3000</p>
+            <p>4000</p>
+            <p>5000</p>
+            <p>6000</p>
+            <p>7000</p>
+            <p>8000</p>
+            <p>9000</p>
+            <p>1000</p>
+          </div>
+        </div>
+        <div className='row'>
+          <h3 className='addressFrom title'>From</h3>
+          <div className='values'>
+            <p>1000</p>
+            <p>2000</p>
+            <p>3000</p>
+            <p>4000</p>
+            <p>5000</p>
+            <p>6000</p>
+            <p>7000</p>
+            <p>8000</p>
+            <p>9000</p>
+            <p>1000</p>
+          </div>
+        </div>
+        <div className='row'>
+          <h4 className='addressTo title'>To</h4>
+          <div className='values'>
+            <p>1000</p>
+            <p>2000</p>
+            <p>3000</p>
+            <p>4000</p>
+            <p>5000</p>
+            <p>6000</p>
+            <p>7000</p>
+            <p>8000</p>
+            <p>9000</p>
+            <p>1000</p>
+          </div>
+        </div>
+        <div className='row'>
+          <h5 className='value title'>Value</h5>
+          <div className='values'>
+            <p>1000</p>
+            <p>2000</p>
+            <p>3000</p>
+            <p>4000</p>
+            <p>5000</p>
+            <p>6000</p>
+            <p>7000</p>
+            <p>8000</p>
+            <p>9000</p>
+            <p>1000</p>
+          </div>
+        </div>
+        <div className='row'>
+          <h6 className='fee title'>Fee</h6>
+          <div className='values'>
+            <p>1000</p>
+            <p>2000</p>
+            <p>3000</p>
+            <p>4000</p>
+            <p>5000</p>
+            <p>6000</p>
+            <p>7000</p>
+            <p>8000</p>
+            <p>9000</p>
+            <p>1000</p>
+          </div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
